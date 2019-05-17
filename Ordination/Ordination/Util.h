bf084501d8f8c5ecca19e31085656ec8c6f4c9d1
Jean <jeanribeiro2004@gmail.com>
2019-05-16 22:50:16 -0300

    D - Ordination - Implementação SelectionSort,InsertionSort e mergeSort

--- Ordination/Ordination/Util.h
+++ Ordination/Ordination/Util.h
@@ -15,14 +15,14 @@ void swap(int *vet, int a,int b) {
 /*
 Função: Ordenar um vetor de inteiros com o algoritimo bubblesort
 */
-float bubbleSort(int *vet, int tamanho)
+float bubbleSort(int *vet, int size)
 {
 	clock_t time;
 	time = clock();
 	bool swapped;
-	for (int i = 0; i < tamanho - 1; i++) {
+	for (int i = 0; i < size - 1; i++) {
 		swapped = false;
-		for (int j = 0 ; j < tamanho - 1; j++) {
+		for (int j = 0 ; j < size - 1; j++) {
 			if (vet[j] > vet[j + 1]) {
 				swap(vet,j,j+1);
 				swapped = true;
@@ -35,4 +35,107 @@ float bubbleSort(int *vet, int tamanho)
 
 	return time / (double)CLOCKS_PER_SEC;
 }
+/*
+Função: Ordenar um vetor de inteiros com o algoritimo selectionSort
+*/
+float selectionSort(int *vet, int size)
+{
+	clock_t time;
+	time = clock();
+
+	int smaller;
+	for (int i = 0; i < size - 1; i++) {
+		smaller = i;
+		for (int j = i+1; j < size; j++) {
+			if (vet[j] < vet[smaller]) {
+				smaller = j;
+			}
+		}
+		if (vet[i] != vet[smaller]) {
+			swap(vet, i, smaller);
+		}			
+	}
+
+	return time / (double)CLOCKS_PER_SEC;
+}
+/*
+Função: Ordenar um vetor de inteiros com o algoritimo insertionSort
+*/
+float insertionSort(int *vet, int size)
+{
+	clock_t time;
+	time = clock();
+
+	int pivot,j;
+	for (int i = 1; i < size; i++) {
+		pivot = vet[i];
+		j = i - 1;
+		while (j >= 0 && vet[j] > pivot) {
+			vet[j + 1] = vet[j];
+			j = j - 1;
+		}
+		vet[j + 1] = pivot;
+	}
+
+	return time / (double)CLOCKS_PER_SEC;
+}
+/*
+Função: Ordenar um vetor de inteiros com o algoritimo mergeSort
+*/
+void merge(int vetor[], int comeco, int meio, int fim) {
+	int com1 = comeco, com2 = meio + 1, comAux = 0, tam = fim - comeco + 1;
+	int *vetAux;
+	vetAux = (int*)malloc(tam * sizeof(int));
+
+	while (com1 <= meio && com2 <= fim) {
+		if (vetor[com1] < vetor[com2]) {
+			vetAux[comAux] = vetor[com1];
+			com1++;
+		}
+		else {
+			vetAux[comAux] = vetor[com2];
+			com2++;
+		}
+		comAux++;
+	}
+
+	while (com1 <= meio) {  //Caso ainda haja elementos na primeira metade
+		vetAux[comAux] = vetor[com1];
+		comAux++;
+		com1++;
+	}
+
+	while (com2 <= fim) {   //Caso ainda haja elementos na segunda metade
+		vetAux[comAux] = vetor[com2];
+		comAux++;
+		com2++;
+	}
+
+	for (comAux = comeco; comAux <= fim; comAux++) {    //Move os elementos de volta para o vetor original
+		vetor[comAux] = vetAux[comAux - comeco];
+	}
+
+	free(vetAux);
+}
+float mergeSort(int *vet, int start,int end)
+{
+	clock_t time;
+	time = clock();
+
+	int medium;
+	if (start < end) {
+		medium = (start + end) / 2;
+		mergeSort(vet, start, medium);
+		mergeSort(vet, medium+1, end);
+		merge(vet, start, medium, end);
+	}
+
+	return time / (double)CLOCKS_PER_SEC;
+}
+
+
+
+
+
+
 #endif
--- Ordination/Ordination/main.cpp
+++ Ordination/Ordination/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include "Util.h"
-#define MAX 5
+#define MAX 10
 using namespace std;
 
 int main() {
-	int vector[MAX] = { 5, 3, 8, 9, 2 };
+	int vector[MAX] = { 5, 3, 8, 9, 2,1,7,10,4,6 };
+
 /*Algoritimo BubbleSort*/
 	cout <<"Tempo BubbleSort: "<< bubbleSort(vector, MAX)<<" s;"<< endl<<"Resultado: ";
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < MAX; i++)
 		cout << vector[i]<<",";
 	cout << endl;
 /*END*/
-/*Algoritimo BubbleSort*/
-	cout << "Tempo SelectionSort: " << bubbleSort(vector, MAX) << " s;" << endl << "Resultado: ";
-	for (int i = 0; i < 5; i++)
+/*Algoritimo SelectionSort*/
+	cout << "Tempo SelectionSort: " << selectionSort(vector, MAX) << " s;" << endl << "Resultado: ";
+	for (int i = 0; i < MAX; i++)
+		cout << vector[i] << ",";
+	cout << endl;
+/*END*/
+/*Algoritimo InsertionSort*/
+	cout << "Tempo InsertionSort: " << insertionSort(vector, MAX) << " s;" << endl << "Resultado: ";
+	for (int i = 0; i < MAX; i++)
+		cout << vector[i] << ",";
+	cout << endl;
+/*END*/
+/*Algoritimo MergeSort*/
+	cout << "Tempo MergeSort: " << mergeSort(vector,0, MAX-1) << " s;" << endl << "Resultado: ";
+	for (int i = 0; i < MAX; i++)
 		cout << vector[i] << ",";
 	cout << endl;
 /*END*/
